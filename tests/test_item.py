8e0f6d17b6e4c3ae52a12da215b5dbdffd560e19
Ilya Irmashev <ilya290397@gmail.com>
2023-09-11 22:14:07 +0400

    Добавил классу 'Item' магические методы '__repr__' и '__str__' Написал тесты для '__repr__' и '__str__'

--- tests/test_item.py
+++ tests/test_item.py
@@ -33,3 +33,13 @@ def test_string_to_number():
     assert Item.string_to_number('6') == 6
     assert Item.string_to_number('7.0') == 7
     assert Item.string_to_number('8.7') == 8
+
+
+def test_repr():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+
+def test_str():
+    item1 = Item("Смартфон", 10000, 20)
+    assert str(item1) == 'Смартфон'
